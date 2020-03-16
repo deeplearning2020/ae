b19b84323067992235c967d18eaf1d341a53d7ef
goyeahia <goyeahia@outlook.com>
2020-03-16 20:13:10 +0600

    add files

---
+++ vae/train.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras import backend as K
+import os
+import numpy as np
+from PIL import Image
+from tensorflow.python.keras.preprocessing.image import load_img
+from models import Darknet19Encoder, Darknet19Decoder
+
+class AutoEncoder(object):
+    def __init__(self, encoderArchitecture, 
+                 decoderArchitecture):
+        self.encoder = encoderArchitecture.model
+        self.decoder = decoderArchitecture.model
+        self.ae = Model(self.encoder.inputs, self.decoder(self.encoder.outputs))
+
+def test():
+    inputShape = (256, 256, 3)
+    batchSize = 8
+    latentSize = 100
+    img = load_img(os.path.join(os.path.dirname(__file__), '..','images', 'img.jpg'), target_size=inputShape[:-1])
+    img.show()
+    img = np.array(img, dtype=np.float32) * (2/255) - 1
+    img = np.array([img]*batchSize)
+    encoder = Darknet19Encoder(inputShape, latentSize=latentSize, latentConstraints='bvae', beta=69)
+    decoder = Darknet19Decoder(inputShape, latentSize=latentSize)
+    bvae = AutoEncoder(encoder, decoder)
+    bvae.ae.compile(optimizer='adam', loss='mean_absolute_error')
+    while True:
+        bvae.ae.fit(img, img,
+                    epochs=100,
+                    batch_size=batchSize)
+        latentVec = bvae.encoder.predict(img)[0]
+        pred = bvae.ae.predict(img)
+        pred = np.uint8((pred + 1)* 255/2)
+        pred = Image.fromarray(pred[0])
+        pred.save("reconstruced_image.png")
+        pred.show()
+
+if __name__ == "__main__":
+    test()
