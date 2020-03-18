7278821f25cf730febefae9d550a437fba2690f1
goyeahia <goyeahia@outlook.com>
2020-03-19 01:30:39 +0600

    py

--- vae/train.py
+++ vae/train.py
@@ -39,7 +39,7 @@ def step_decay(epoch):
 
 def test():
     inputShape = (256, 256, 3)
-    batchSize = 8
+    batchSize = 4
     latentSize = 100
     img = load_img(os.path.join(os.getcwd(), 'images', 'img.bmp'),target_size=inputShape[:-1])
     img = np.array(img, dtype=np.float32) * (2/255) - 1
