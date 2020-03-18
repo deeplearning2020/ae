996fba8d41f2d3d336cb26b2b5272c2de98d9d38
goyeahia <goyeahia@outlook.com>
2020-03-19 00:08:47 +0600

    add files

--- vae/train.py
+++ vae/train.py
@@ -41,7 +41,7 @@ def test():
     inputShape = (256, 256, 3)
     batchSize = 8
     latentSize = 100
-    img = load_img(os.path.join(os.path.dirname(__file__), '..','images', 'img.bmp'),target_size=inputShape[:-1])
+    img = load_img(os.path.join(os.getcwd(), 'images', 'img.bmp'),target_size=inputShape[:-1])
     img = np.array(img, dtype=np.float32) * (2/255) - 1
     img = np.array([img]*batchSize)
     new_img = load_img(os.path.join(os.getcwd(),'cropped', 'img.bmp'),target_size=inputShape[:-1])
