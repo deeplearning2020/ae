9c16ae027f2a0bb0a5418bf7998914a4dc830bc6
goyeahia <goyeahia@outlook.com>
2020-03-21 12:48:28 +0600

    4rg

--- vae/train.py
+++ vae/train.py
@@ -57,7 +57,7 @@ def test():
     #filepath = "best-model.hdf5"
     #checkpoint = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
     #callbacks_list = [checkpoint]
-    bvae.ae.fit(img, new_img,
+    bvae.ae.fit(new_img,img,
                 epochs=5000,
                 batch_size=batchSize,callbacks = [es])
     #bvae.ae.save('sr.h5')
