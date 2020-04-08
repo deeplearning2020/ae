26d23c14c5604ce1e643d5852f92675df99c27ac
goyeahia <yeahia.ruet@gmail.com>
2020-04-08 13:36:43 +0600

    refurbishing - 1

--- vae/train.py
+++ vae/train.py
@@ -10,26 +10,27 @@ from models import vgg_encoder, vgg_decoder
 from matplotlib import pyplot as plt
 
 class variational_autoencoder(object):
-    def __init__(self, encoderArchitecture, 
-                 decoderArchitecture):
-        self.encoder = encoderArchitecture.model
-        self.decoder = decoderArchitecture.model
+    def __init__(self, encoder_architecture, 
+                 decoder_architecture):
+        self.encoder = encoder_architecture.model
+        self.decoder = decoder_architecture.model
         self.ae = Model(self.encoder.inputs, self.decoder(self.encoder.outputs))
 
 
 def main():
+
     inputShape = (256, 256, 3)
     batchSize = 8
     latentSize = 400
 
-    hr_image = load_img(os.path.join(os.getcwd(),'HR_image','HR.bmp'),target_size=inputShape[:-1])
+    hr_image = load_img(os.path.join(os.getcwd(),'hr_image','HR.bmp'),target_size=inputShape[:-1]) ## loading the high-resolution image
     hr_image = np.array(hr_image, dtype=np.float32) * (2/255) - 1
-    hr_image = np.array([hr_image]*batchSize)
+    hr_image = np.array([hr_image]*batchSize) ## creating fake batches
 
 
-    lr_image = load_img(os.path.join(os.getcwd(),'LR_image','LR.bmp'),target_size=inputShape[:-1])
+    lr_image = load_img(os.path.join(os.getcwd(),'lr_image','LR.bmp'),target_size=inputShape[:-1]) ## loading the low-resolution image
     lr_image = np.array(lr_image, dtype=np.float32) * (2/255) - 1
-    lr_image = np.array([lr_image]*batchSize)
+    lr_image = np.array([lr_image]*batchSize) ## creating fake batches
 
 
 
@@ -41,25 +42,25 @@ def main():
     
     bvae.ae.compile(optimizer = 'adam', loss = 'mse')
     
-    es = EarlyStopping(monitor = 'loss', mode = 'min', verbose = 1, patience = 70)
+    es = EarlyStopping(monitor = 'loss', mode = 'min', verbose = 1, patience = 70) ## early stopping to prevent overfitting
+
     history = bvae.ae.fit(lr_image,hr_image,
                 epochs = 2000,
                 batch_size = batchSize,callbacks = [es])
 
+    """ reconstrucing high-resolution image from the low-resolution image """
     pred = bvae.ae.predict(lr_image)
     pred = np.uint8((pred + 1)* 255/2)
     pred = Image.fromarray(pred[0])
     pred.save("reconstructed_HR_image.png")
-
-
-    """
+    
+    """ plotting training loss """
     plt.plot(history.history['loss'])
     plt.title('Reconstruction loss on a SET12 image sample')
     plt.ylabel('Training Loss')
     plt.xlabel('Epoch')
     plt.legend(['Train'], loc='lower right')
     plt.savefig("loss.pdf")
-    """
 
 if __name__ == "__main__":
     main()
